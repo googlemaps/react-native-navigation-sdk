fb28908575645804c2c0fea2326b4f0a13319f67
Ville Välimaa <ville.valimaa@codemate.com>
2024-11-12 18:24:05 +0800

    test: detox base config

--- example/e2e/navigation_ui.test.js
+++ example/e2e/navigation_ui.test.js
@@ -14,29 +14,15 @@
  * limitations under the License.
  */
 
-describe('Example', () => {
-  /*
-  beforeAll(async () => {
-    await device.launchApp();
-  });
+import { initializeNavigationPage } from './shared.js';
+import { element, by } from 'detox';
 
+describe('Initialization tests', () => {
   beforeEach(async () => {
-    await device.reloadReactNative();
+    await initializeNavigationPage();
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
+  it('should open a navigation page', async () => {
+    await expect(element(by.text('Margin'))).toBeVisible();
   });
-
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
-  });
-  */
-  it('placeholder test', async () => {});
 });
