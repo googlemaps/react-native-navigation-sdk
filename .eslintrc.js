b5992ac84de20b48e205a1de9035d187d7fd5dde
Joonas Kerttula <joonas.kerttula@codemate.com>
2025-09-11 14:46:54 +0300

    fix: package dependencies and eslint version issues

---
+++ .eslintrc.js
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    'es2021': true,
+    'node': true,
+    'react-native/react-native': true,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'react-native',
+    'import',
+    'prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-native/all',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react-native/sort-styles': 'off',
+    'react-native/no-color-literals': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'warn',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Allow unused vars starting with _
+    'import/default': 'off', // Disable for now due to react-native-select-dropdown issues
+  },
+  settings: {
+    'react': {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  overrides: [
+    // Configuration files can use require()
+    {
+      files: ['**/*.config.js', '**/babel.config.js'],
+      rules: {
+        '@typescript-eslint/no-require-imports': 'off',
+      },
+    },
+    // Test files
+    {
+      files: ['**/*.test.js', '**/*.test.ts', '**/e2e/**/*.js'],
+      env: {
+        jest: true,
+      },
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+    // Example directory - less strict for demo code
+    {
+      files: ['example/**/*'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+  ],
+};
